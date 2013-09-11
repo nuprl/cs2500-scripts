3f34ca1df78864f71beab8b9022c891333700a36
William J. Bowman <wjb@williamjbowman.com>
2013-09-11 15:55:18 -0400

    * Added send problem sets to graders   refresh.rkt * Added interface to get problem-sets and filter active problem sets   cs2500-grading/problem-set.rkt

--- refresh.rkt
+++ refresh.rkt
@@ -1,3 +1,6 @@
 #lang racket
-(require "cs2500-grading/problem-set.rkt")
+(require "cs2500-grading/problem-set.rkt"
+         "cs2500-grading/grading.rkt"
+         "cs2500-grading/config.rkt")
+(send-assignments-to-graders (active-problems-sets (problem-sets)))
 (write-current-problem-sets!)
