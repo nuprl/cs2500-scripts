4602c46ab3b45ec9c8cc5d4064447fe6d4eaeb81
William J. Bowman <wjb@williamjbowman.com>
2013-09-11 16:13:18 -0400

    * Fixed mismatch name vs definition   refresh.rkt

--- refresh.rkt
+++ refresh.rkt
@@ -2,5 +2,5 @@
 (require "cs2500-grading/problem-set.rkt"
          "cs2500-grading/grading.rkt"
          "cs2500-grading/config.rkt")
-(send-assignments-to-graders (active-problems-sets (problem-sets)))
+(send-assignments-to-graders (active-problem-sets (problem-sets)))
 (write-current-problem-sets!)
