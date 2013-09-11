d9e66915e9e458f76e0a4d555511f973d93b96a2
William J. Bowman <wjb@williamjbowman.com>
2013-09-10 23:15:24 -0400

    * Changed problem set directory to tar "grading", where things get   unpacked to.   cs2500-grading/grading.rkt

--- cs2500-grading/config.rkt
+++ cs2500-grading/config.rkt
@@ -18,6 +18,8 @@
 ;; string
 (define server-dir (make-parameter (get-config 'server-dir)))
 ;; string
+(define grade-server-dir (make-parameter (get-config 'grade-server-dir)))
+;; string
 (define smtp-server (make-parameter (get-config 'smtp-server)))
 ;; string
 (define smtp-port (make-parameter (get-config 'smtp-port)))
@@ -27,8 +29,6 @@
 ;; grader-name, problem-set-name -> string
 (define message-body (make-parameter (eval (get-config 'message-body)
                                            (make-base-namespace))))
-;; string
-(define grade-server-dir (make-parameter (get-config 'grade-server-dir)))
 
 ;; TODO: Make parameters
 (define gradebook-path (build-path (current-directory) "gradebook.rktd"))
